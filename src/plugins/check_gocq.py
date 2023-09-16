ba1384a4ae58fe02469ed5849283d7b09542c084
angjustinl <77139032@qq.com>
2023-09-16 12:39:59 +0800

    v2.0 rebuild

---
+++ src/plugins/check_gocq.py
@@ -0,0 +1,56 @@
+import asyncio
+import nonebot
+import subprocess
+
+from nonebot.log import logger
+from nonebot import require, on_command
+
+
+try:
+    scheduler = require("nonebot_plugin_apscheduler")
+    from nonebot_plugin_apscheduler import scheduler
+except Exception:
+    scheduler = None
+
+logger.opt(colors=True).info(
+    "已检测到软依赖<y>nonebot_plugin_apscheduler</y>, <g>开启gocq检测</g>"
+    if scheduler
+    else "未检测到软依赖<y>nonebot_plugin_apscheduler</y>，<r>禁用gocq检测</r>"
+)
+
+restart_qsign = on_command('restart_qsign_bot1',aliases={'bot1_re'}, priority=5, block=True)
+
+
+async def qsign_livecheck():
+    msg_ids = []
+    try:
+        msg_id = (await nonebot.get_bot().send_group_msg(group_id=696748432,message='bot存活检测中,若此条未撤回,则bot进行一次重启'))["message_id"]
+        msg_ids.append(msg_id)
+        await asyncio.sleep(30)
+        try:
+                for msg_id in msg_ids:
+                    await nonebot.get_bot().delete_msg(message_id=msg_id)
+        except:
+                pass
+    except Exception as e:
+        try:
+            subprocess.run(["docker", "restart", "qsign"])
+            asyncio.sleep(30)
+            await nonebot.get_bot().send_group_msg(group_id=696748432,message='重启,上次下线原因为: '+str(e))
+        except Exception:
+            logger.critical("gocq出错")
+            raise SystemExit
+
+@restart_qsign.handle()
+async def restarting_qsign():   
+    await qsign_livecheck()
+    pass
+
+if scheduler:
+
+    scheduler.add_job(
+                qsign_livecheck,
+                "interval",
+                hours=1,
+                id='qsign1_livecheck'
+            )
---
+++ src/plugins/wallpaper/get.py
@@ -0,0 +1,15 @@
+import asyncio
+from socket import timeout
+import httpx
+import base64
+ 
+async def get_msgurl(url):
+    async with httpx.AsyncClient(verify=False, timeout=None) as client:
+      resp = await client.get(
+        url, headers={"Referer": "https://weibo.com/"}, timeout=30
+      )
+      page = resp.content
+      #res = re.compile(r'src="(http.+?.jpg)"')
+      #reg = re.findall(res, page)
+      #print(reg)
+      return page
