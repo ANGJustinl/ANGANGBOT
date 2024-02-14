9ffefe7f74d60f32c5d2c5753447eab2b76a1977
angjustinl <angjustin@163.com>
2024-02-14 20:05:52 +0800

    :bug: hotfix

--- bot.py
+++ bot.py
@@ -10,30 +10,29 @@ app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
 
-nonebot.load_builtin_plugins('echo')
+nonebot.load_builtin_plugins("echo")
 require("nonebot_plugin_guild_patch")
 
-nonebot.load_plugin('nonebot_plugin_pixivbot')
-nonebot.load_plugin('nonebot_plugin_setu4')
-nonebot.load_plugin('nonebot_plugin_tarot')
-nonebot.load_plugin('nonebot_plugin_fortune')
-
-nonebot.load_plugin('nonebot_plugin_PicMenu')
-nonebot.load_plugin('nonebot_plugin_weather_lite')
+nonebot.load_plugin("nonebot_plugin_pixivbot")
+nonebot.load_plugin("nonebot_plugin_tarot")
+# nonebot.load_plugin('nonebot_plugin_fortune')
+nonebot.load_plugin("nonebot_plugin_PicMenu")
+nonebot.load_plugin("nonebot_plugin_weather_lite")
 
 
-#nonebot.load_plugin('nonebot_plugin_bili_push')
+# nonebot.load_plugin('nonebot_plugin_bili_push')
 
 
-logger.success("\n"
-"   #    #     #  #####     #    #     #  #####  ######  ####### ####### \n"
-"  # #   ##    # #     #   # #   ##    # #     # #     # #     #    #    \n"
-" #   #  # #   # #        #   #  # #   # #       #     # #     #    #    \n"
-"#     # #  #  # #  #### #     # #  #  # #  #### ######  #     #    #    \n"
-"####### #   # # #     # ####### #   # # #     # #     # #     #    #    \n"
-"#     # #    ## #     # #     # #    ## #     # #     # #     #    #    \n"
-"#     # #     #  #####  #     # #     #  #####  ######  #######    #    \n"
-'昂昂bot部署成功'
+logger.success(
+    "\n"
+    "   #    #     #  #####     #    #     #  #####  ######  ####### ####### \n"
+    "  # #   ##    # #     #   # #   ##    # #     # #     # #     #    #    \n"
+    " #   #  # #   # #        #   #  # #   # #       #     # #     #    #    \n"
+    "#     # #  #  # #  #### #     # #  #  # #  #### ######  #     #    #    \n"
+    "####### #   # # #     # ####### #   # # #     # #     # #     #    #    \n"
+    "#     # #    ## #     # #     # #    ## #     # #     # #     #    #    \n"
+    "#     # #     #  #####  #     # #     #  #####  ######  #######    #    \n"
+    "昂昂bot部署成功"
 )
 
 nonebot.load_from_toml("pyproject.toml")
