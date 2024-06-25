59de09cc68ba9b4964a18b97e90cdba96ab60a0a
angjustinl <96008766+ANGJustinl@users.noreply.github.com>
2024-06-25 11:04:53 +0800

    🚧v3.0.0-dev2

--- src/plugins/nonebot_plugin_animalVoice/cheru_main.py
+++ src/plugins/nonebot_plugin_animalVoice/cheru_main.py
@@ -3,10 +3,16 @@ from nonebot.params import Arg
 from nonebot.adapters.onebot.v11 import MessageEvent, Message, Bot
 from nonebot.typing import T_State
 
+from .config import plugin_config as Config
 from .Cheru import cheru2str, str2cheru
 
-cherulizing = on_command("切噜一下", aliases={"cherulize"}, priority=5, block=True)
-decherulizing = on_command("切噜～", aliases={"decherulize"}, priority=5, block=True)
+cherulizing = on_command(
+    Config.customize_cmd_cherulizing, aliases={"cherulize"}, priority=5, block=True
+)
+decherulizing = on_command(
+    Config.customize_cmd_decherulizing, aliases={"decherulize"}, priority=5, block=True
+)
+if_at_sender = Config.at_sender
 
 
 @cherulizing.handle()
@@ -19,7 +25,7 @@ async def _():
 async def get_image(bot: Bot, state: T_State, msgs: Message = Arg()):
     msg_input = msgs
     if len(msg_input) > 500:
-        await bot.send("切、切噜太长切不动勒切噜噜...", at_sender=True)
+        await bot.send("切、切噜太长切不动勒切噜噜...", at_sender=if_at_sender)
         return
     state["msg"] = msg_input
 
@@ -30,16 +36,14 @@ async def _(bot: Bot, state: T_State, event: MessageEvent):
     try:
         msg = str2cheru(msg_input)
     except Exception as e:
-        await cherulizing.finish(
-            "切、切噜报错切不动勒切噜噜{}".format(e), reply_message=True
-        )
+        await cherulizing.finish("切、切噜报错切不动勒切噜噜{}".format(e), reply_message=True)
 
     msgs = []
     msgs.append(
         {
             "type": "node",
             "data": {
-                "name": "昂昂切噜bot",
+                "name": "风间千爱瑠",
                 "uin": bot.self_id,
                 "content": "切噜～♪\n{}".format(msg),
             },
@@ -59,7 +63,7 @@ async def _(state: T_State):
 async def get_image(bot: Bot, state: T_State, msgs: Message = Arg()):
     msg_input = msgs
     if len(msg_input) > 1501:
-        await bot.send("切、切噜太长切不动勒切噜噜...", at_sender=True)
+        await bot.send("切、切噜太长切不动勒切噜噜...", at_sender=if_at_sender)
         return
     state["msg"] = msg_input
 
@@ -70,15 +74,13 @@ async def _(bot: Bot, state: T_State, event: MessageEvent):
     try:
         msg = "你的的切噜噜是：\n{}".format(cheru2str(msg_input))
     except Exception as e:
-        await decherulizing.finish(
-            "切、切噜报错切不动勒切噜噜{}".format(e), reply_message=True
-        )
+        await decherulizing.finish("切、切噜报错切不动勒切噜噜{}".format(e), reply_message=True)
 
     msgs = []
     msgs.append(
         {
             "type": "node",
-            "data": {"name": "昂昂切噜bot", "uin": bot.self_id, "content": msg},
+            "data": {"name": "风间千爱瑠", "uin": bot.self_id, "content": msg},
         }
     )
     await bot.call_api("send_group_forward_msg", group_id=event.group_id, messages=msgs)
